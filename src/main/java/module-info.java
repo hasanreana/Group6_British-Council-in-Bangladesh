297d6171912c1cbcfe412343936bbe5c4dea2d95
ReanaAhmed <1910961@iub.edu.bd>
2025-07-29 12:47:31 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.group6_britishcouncilinbangladesh {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.group6_britishcouncilinbangladesh to javafx.fxml;
+    exports com.example.group6_britishcouncilinbangladesh;
+}
