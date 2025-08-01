deeb477f7d9f1f70b5c74d95d7955439de27202f
s-munna <2111315@iub.edu.bd>
2025-08-01 23:44:30 +0600

    user7&8

---
+++ src/main/java/com/example/group6_britishcouncilinbangladesh/Solaiman_Munna/DashboardIELTScordController.java
@@ -0,0 +1,46 @@
+package com.example.group6_britishcouncilinbangladesh.Solaiman_Munna;
+
+import javafx.event.ActionEvent;
+
+public class DashboardIELTScordController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ProcessPaymentsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RegisterStudentsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutButtonICD(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewPerformanceReportsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ManageAccountButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ManageTestSessionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void VerifyStudentIdentityButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UploadTestResultsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void IssueAdmitCardsButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/group6_britishcouncilinbangladesh/Solaiman_Munna/ManageTestSessionIECOrdController.java
@@ -0,0 +1,20 @@
+package com.example.group6_britishcouncilinbangladesh.Solaiman_Munna;
+
+public class ManageTestSessionIECOrdController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void AddNewSessionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewExistingTestSessionsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButton(ActionEvent actionEvent) {
+    }
+}
